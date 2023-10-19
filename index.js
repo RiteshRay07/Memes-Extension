e0fbbd820d9c73e60a15d7e039bef9be959b5d1b
riteshray07 <DeveloperRiteshRay@gmail.com>
2023-10-19 15:51:15 +0530

    init code

--- index.js
+++ index.js
@@ -1,7 +1,18 @@
+const meme_result = document.getElementById("mymemes");
+const btn = document.getElementById("btn");
 
-fetch('https://meme-api.herokuapp.com/gimme')           //api for the get request
-    .then(response => response.json())
-    .then(data => console.log(data));
+async function getRandomMeme() {
+  //   fetch("https://meme-api.com/gimme/wholesomememes")
+  const response = await fetch(`https://meme-api.com/gimme`);
 
+  const data = await response.json();
 
+  meme_result.innerHTML = `<img src="${data.url}"/>`;
+  // console.log(data);
+}
 
+btn.addEventListener("click", function nextmMeme() {
+  getRandomMeme();
+});
+
+getRandomMeme();
